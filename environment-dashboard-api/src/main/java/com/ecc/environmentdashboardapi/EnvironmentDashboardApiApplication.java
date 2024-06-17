c6120feae0795c518fe0ba8aecaac8743d88c612
Nolan-James <nj@nolanjames.me>
2024-06-17 11:09:25 +0100

    api setup

---
+++ environment-dashboard-api/src/main/java/com/ecc/environmentdashboardapi/EnvironmentDashboardApiApplication.java
@@ -0,0 +1,13 @@
+package com.ecc.environmentdashboardapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnvironmentDashboardApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnvironmentDashboardApiApplication.class, args);
+    }
+
+}
---
+++ environment-dashboard-api/src/test/java/com/ecc/environmentdashboardapi/EnvironmentDashboardApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecc.environmentdashboardapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnvironmentDashboardApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
